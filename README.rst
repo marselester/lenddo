8496aca07b85994db628f86f5903ac410990184b
marselester <marselester@ya.ru>
2015-03-26 17:34:01 +0700

    Add meta files

---
+++ README.rst
@@ -0,0 +1,13 @@
+======
+Lenddo
+======
+
+Lenddo is a Python API client for Lenddo.com.
+
+Tests
+-----
+
+.. code-block:: console
+
+    $ pip install -r requirements.txt
+    $ tox
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='Lenddo',
+    version='0.0.1',
+    packages=['lenddo'],
+    author='Marsel Mavletkulov',
+    author_email='marselester@ya.ru',
+    url='https://github.com/marselester/lenddo',
+    description='Python API client for Lenddo.com',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'requests>=2',
+        'python-dateutil>=2.1',
+        'six'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
